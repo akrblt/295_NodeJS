07e2fc0015f11e4c58dc6b2feaee5a300e229f77
akrblt <a.karabulut070@gmail.com>
2025-08-19 15:14:42 +0200

    feat: math operation

---
+++ exercice_js.mjs
@@ -0,0 +1,27 @@
+// simple calculator
+
+function add(a,b) {
+    return a+b;
+}
+
+let add2=(a,b)=>a+b;
+
+function subtract(a,b){
+    return  a-b;
+}
+
+let substract2=(a,b)=>a-b;
+
+function multiply(a,b){
+    return a*b;
+}
+
+let multiply2=(a,b)=>a*b;
+
+function divide(a,b){
+    return a/b;
+}
+let divide2=(a,b)=>a/b;
+
+console.log(add(2,3));
+console.log(add2(2,3));
