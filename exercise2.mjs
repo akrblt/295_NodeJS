08eb354f9b0bcd86c19f0eee9bcebcfe1f571002
akrblt <a.karabulut070@gmail.com>
2025-08-25 09:19:25 +0200

    feat : basic calculateur

--- exercise2.mjs
+++ exercise2.mjs
@@ -45,3 +45,21 @@ console.log("\n Morceaux disponibles");
 showPlaylist();
 
 // Exercise 3 Boucle
+for (let i = 0; i < 10; i++) {
+    console.log(String(i).repeat(i));
+}
+// Exercise 4
+const add=(a,b)=>a+b;
+const sub=(a,b)=>a-b;
+const mul=(a,b)=>a*b;
+const div=(a,b)=>{
+    if (b==0){
+        throw new   Error("Division par zéro interdite");
+    }
+    return a/b;
+};
+console.log(add(2,3));
+console.log(mul(2,3));
+console.log(div(2,3));
+console.log(sub(2,3));
+//console.log(div(2,0));
